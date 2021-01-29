b74f967b631eadb51a51a2ed6f3a52253b44db40
Kieren Hinch <khinch-github@nylonmoon.com>
2021-01-29 16:26:59 +0000

    Update server to run on random available port instead of 8000.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    println!("Attempting to bind to address: 127.0.0.1:{}", listener.local_addr().unwrap().port());
+    run(listener)?.await
 }
